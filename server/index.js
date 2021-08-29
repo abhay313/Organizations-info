c10bafcb7a1ef515c4d1c840aa3aff66196ca4cb
abhay313 <abhayiiitk@gmail.com>
2021-08-29 23:32:06 +0530

    fixing bugs

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import postRoutes from './routes/posts.js';
@@ -8,8 +7,7 @@ import dotenv from 'dotenv';
 const app = express()
 dotenv.config();
 
-app.use(bodyParser.json({limit: "5mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "5mb", extended: true}));
+app.use(express.json())
 app.use(cors());
 
 app.use('/posts', postRoutes);
